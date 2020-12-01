92c27b66988fb988bc7fb765f42b1b2a2af3b0cf
ddcovery <antonio@abrevia.net>
2020-12-01 10:48:31 +0100

    * run using python3 instead python

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-
 #!/usr/bin/env sh
-crystal sorted.cr --release && sleep 2s && rdmd sorted.d --release && sleep 2s && node sorted.js && sleep 2s && python sorted.py
+crystal sorted.cr --release && sleep 2s && rdmd sorted.d --release && sleep 2s && node sorted.js && sleep 2s && python3 sorted.py
