37677f9dde3440f9c06fc9d75873c30deff4f76d
ddcovery <antonio@abrevia.net>
2020-12-03 11:07:59 +0100

    * test.sh - Exports to a "results.csv" file. - Adds compiler name as CSV first column.

--- test.sh
+++ test.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env sh
-echo "D (DMD)"
-dmd -O -release -run sorted.d
-echo "D (LCD)"
-ldc2 -O5 -release -enable-cross-module-inlining --run sorted.d 
-echo "Crystal"
-crystal run sorted.cr --release 
-echo "javascript (node)"
-node sorted.js 
-echo "python"
-python3 sorted.py
+echo "compiler,lang,size,ms"
+ldc2 -O5 -release -enable-cross-module-inlining --run sorted.d | sed "s/^/\"ldc2\",/"
+crystal run sorted.cr --release  | sed "s/^/\"crystal\",/"
+node sorted.js  | sed "s/^/\"node\",/"
+dmd -O -release -run sorted.d | sed "s/^/\"dmd\",/"
+python3 -OO sorted.py | sed "s/^/\"python3\",/"
+
